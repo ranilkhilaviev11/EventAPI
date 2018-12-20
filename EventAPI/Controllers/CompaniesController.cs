65eae37a73d1d3a8464bf8017c051b78cd1e959e
ranilkhilaviev11 <44546578+ranilkhilaviev11@users.noreply.github.com>
2018-12-20 23:28:49 +0300

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	new file:   EventAPI/Controllers/CompaniesController.cs
    	new file:   EventAPI/DTO/CompanyDTO.cs
    
    	modified:   .vs/EventAPI/DesignTimeBuild/.dtbcache
    	modified:   .vs/EventAPI/v15/.suo
    	modified:   .vs/EventAPI/v15/Server/sqlite3/storage.ide-shm
    	modified:   .vs/EventAPI/v15/Server/sqlite3/storage.ide-wal
    	modified:   EventAPI/obj/Debug/netcoreapp2.1/EventAPI.assets.cache
    	modified:   EventAPI/obj/EventAPI.csproj.nuget.cache
    	modified:   EventAPI/obj/EventAPI.csproj.nuget.g.props
    	modified:   EventAPI/obj/project.assets.json

---
+++ EventAPI/Controllers/CompaniesController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EventAPI;
+using AutoMapper;
+
+namespace EventAPI.Controllers
+{
+    [Route("api/company")]
+    [ApiController]
+    public class CompaniesController : ControllerBase
+    {
+        MailingContext _context = new MailingContext();
+        private readonly List<Company> _companies;
+        public CompaniesController()
+        {
+            _companies = _context.Company.ToList();
+        }
+
+        // GET: api/Companies
+        [HttpGet]
+        public IEnumerable<CompanyDTO> GetCompany()
+        { 
+            List<CompanyDTO> list_obj = Mapper.Map(_companies, new List<CompanyDTO>());
+            return list_obj;
+        }
+
+        // GET: api/Companies/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCompany([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var _company = await _context.Company
+             .SingleOrDefaultAsync(u => u.Id == id);
+            var model = Mapper.Map<CompanyDTO>(_company);
+
+            if (_company == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(model);
+        }
+
+        // PUT: api/Companies/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCompany([FromRoute] int id, [FromBody] CompanyDTO _company)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != _company.Id)
+            {
+                return BadRequest();
+            }
+           
+
+            try
+            {
+                Company company_map = Mapper.Map<Company>(_company);
+                _context.Entry(company_map).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                var message = ex.Message;
+                if (!CompanyExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Companies
+        [HttpPost]
+        public async Task<IActionResult> PostCompany([FromBody] CompanyDTO _company)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                var config = new MapperConfiguration(cfg => cfg.CreateMap<CompanyDTO, Company>());
+                var mapper = config.CreateMapper();
+                Company company_map = mapper.Map<Company>(_company);
+                _context.Company.Add(company_map);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (CompanyExists(_company.Id))
+                {
+                    return new StatusCodeResult(StatusCodes.Status409Conflict);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetCompany", new { id = _company.Id }, _company);
+        }
+
+        // DELETE: api/Companies/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCompany([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var _company = await _context.Company
+           .SingleOrDefaultAsync(u => u.Id == id);
+            var model = Mapper.Map<CompanyDTO>(_company);
+            if (_company == null)
+            {
+                return NotFound();
+            }
+
+            _context.Company.Remove(_company);
+            await _context.SaveChangesAsync();
+
+            return Ok(model);
+        }
+
+        private bool CompanyExists(int id)
+        {
+            return _context.Company.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ EventAPI/DTO/CompanyDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventAPI
+{
+    public class CompanyDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CompanyFieldOfActivity { get; set; }
+        public int MainOfficeCountryId { get; set; }
+    }
+}
